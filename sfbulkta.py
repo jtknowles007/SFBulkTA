567b659bb38e4e3cd2ec95a8ef34bf03bb4f5f0a
jtknowles007 <jtknowles007@gmail.com>
2018-10-24 12:56:28 -0400

    Initial commit

---
+++ sfbulkta.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+#
+################################################################################
+#
+# Author: John T. Knowles, RN, RHIA
+# Date: October 24, 2018
+# Version: 0.1
+#
+################################################################################
+#
+'''
+SFBulkTA: Automate adding multiple TA instances to SalesForce database
+'''
