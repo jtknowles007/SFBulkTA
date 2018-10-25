016fdba50b27e42032f0d0ad60e19182180e010b
jtknowles007 <jtknowles007@gmail.com>
2018-10-25 09:48:48 -0400

    Login and form-fill basics

--- sfbulkta.py
+++ sfbulkta.py
@@ -11,3 +11,100 @@
 '''
 SFBulkTA: Automate adding multiple TA instances to SalesForce database
 '''
+################################################################################
+# IMPORT
+################################################################################
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from credentials import *
+################################################################################
+# Login to website
+################################################################################
+
+# What I've learned:
+#
+# There can be no other instances of chrome open when running this script.
+# If there are, there will be errors thrown attempting to login and the browser
+# will get stuck on the default homepage.
+#
+# In order to avoid having the SalesForce website repeatedly ask me to verify
+# my identity, it is necessary for the browser to open to a known profile, in
+# this case it's my default profile.  We accomplish this by passing options to
+# the browser startup specifying which profile to use.  Once we do this, the 
+# website will ask for verification once, then will remember that again until
+# the cache and cookies are removed.  Then it will ask again, just like normal.
+#
+# In order to make the chrome browser run maximized to ensure that as many 
+# page elements are visible to Selenium as possible, it is necessary to add
+# --kiosk as an argument to the browser instance.
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--user-data-dir=/home/john/.config/google-chrome")
+chrome_options.add_argument("--kiosk")
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("https://na74.salesforce.com/00T/e?title=Call&what_id=a0Ao000000YTmUX&followup=1&tsk5=Call&retURL=%2Fa0Ao000000YTmUX")
+
+username = driver.find_element_by_id("username")
+password = driver.find_element_by_id("password")
+
+username.send_keys(user)
+password.send_keys(pswd)
+
+driver.find_element_by_name("Login").click()
+
+###############################################################################
+# Open a new Task
+###############################################################################
+
+# What I've learned:
+
+subject = driver.find_element_by_id("tsk5")
+subject.clear()
+subject.send_keys("Technical Assistance")
+
+name = driver.find_element_by_id("tsk2")
+name.send_keys("Cody Mullen")
+
+duedate = driver.find_element_by_id("tsk4")
+duedate.clear()
+duedate.send_keys("8/8/1997")
+
+endddate = driver.find_element_by_id("00N1J00000EVeQ1")
+endddate.send_keys("8/8/1997")
+
+comments = driver.find_element_by_id("tsk6")
+comments.send_keys("This is a test comment.")
+
+bestpractices = Select(driver.find_element_by_id("00No000000EXDsE"))
+bestpractices.select_by_visible_text("Yes")
+
+sustainability = Select(driver.find_element_by_id("00No000000EXDsL"))
+sustainability.select_by_visible_text("Yes")
+
+hbpeval = Select(driver.find_element_by_id("00No000000EXDsI"))
+hbpeval.select_by_visible_text("Yes")
+
+meds = Select(driver.find_element_by_id("00No000000EXDsJ"))
+meds.select_by_visible_text("Yes")
+
+selfmgmt = Select(driver.find_element_by_id("00No000000EXDsC"))
+selfmgmt.select_by_visible_text("Yes")
+
+lan = Select(driver.find_element_by_id("00No000000EXDsD"))
+lan.select_by_visible_text("Yes")
+
+engagement = Select(driver.find_element_by_id("00No000000EXDsF"))
+engagement.select_by_visible_text("Yes")
+
+literacy = Select(driver.find_element_by_id("00No000000EXDsH"))
+literacy.select_by_visible_text("Yes")
+
+bpguidelines = Select(driver.find_element_by_id("00No000000EXDsG"))
+bpguidelines.select_by_visible_text("Yes")
+
+hhqi = Select(driver.find_element_by_id("00No000000EXDsK"))
+hhqi.select_by_visible_text("Yes")
+
+resources = Select(driver.find_element_by_id("00No000000EXDsB"))
+resources.select_by_visible_text("Yes")
+
